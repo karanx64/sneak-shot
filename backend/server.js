e35635f7922975b78d750d310758761a0ed9d145
karanx64 <karansinghx64@gmail.com>
2025-02-27 20:22:49 +0530

    backend and mongo DB working

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,6 @@ app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
